cdd57d9827edffbc42a907444e25730330a2bdb1
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-16 16:02:32 +0100

    Add exercise 2/C

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,22 @@
 //     - Declare variable identify NUMBER with value 100
 //     - Print in console
 
-const number = 100 //Number
+const number = 100; //Number
 console.log(number);
 
 // EXERCICE 2
 //     - Declare constant identify as PI with value 3.14
 //     - Print in console
 
-const PI = 3.14 //number
-console.log(PI);
+//const PI = 3.14 //number
+//console.log(PI);
 
 // EXERCICE 2 / B
 //      - Assigne to PI value 3.1416.
 //      - What happen ?
 
 //"you can't change the value of a constant because that would be an error"
-const PI = 3.1416
+const PI = 3.1416;
 console.log(PI);
 // Console message: Uncaught SyntaxError: Identifier 'PI' has already been declared (at main.js:20:7)//
 
@@ -26,7 +26,9 @@ console.log(PI);
 //      - Declare variable identify CIRCLE as result of RADIUS * 2 * PI
 //      - Print variable CIRCLE in console
 
-
+    const radius = 8 //number
+    const circle = radius * 2 * PI
+    console.log(circle);
 
 // EXERCICE 3
 //     - Declare variable identify NAME with value as CARLO
