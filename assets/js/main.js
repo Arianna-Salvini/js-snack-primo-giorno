3647f7236a6afaa3e16a84b2d93ec673f04671be
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-16 16:25:06 +0100

    FIx error

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,16 +9,16 @@ console.log(number);
 //     - Declare constant identify as PI with value 3.14
 //     - Print in console
 
-//const PI = 3.14 //number
-//console.log(PI);
+const PI = 3.14 //number
+console.log(PI);
 
 // EXERCICE 2 / B
 //      - Assigne to PI value 3.1416.
 //      - What happen ?
 
 //"you can't change the value of a constant because that would be an error"
-const PI = 3.1416;
-console.log(PI);
+    //const PI = 3.1416; //Number
+    //console.log(PI);
 // Console message: Uncaught SyntaxError: Identifier 'PI' has already been declared (at main.js:20:7)//
 
 //  EXERCICE 2 / C
@@ -26,8 +26,8 @@ console.log(PI);
 //      - Declare variable identify CIRCLE as result of RADIUS * 2 * PI
 //      - Print variable CIRCLE in console
 
-const radius = 8 //number
-const circle = radius * 2 * PI
+const radius = 8 //Number
+const circle = radius * 2 * PI 
 console.log(circle);
 
 // EXERCICE 3
