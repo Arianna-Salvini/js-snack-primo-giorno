b2b5a13191606c93c2747544199f252ccebd1fc6
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-16 15:57:53 +0100

    Add exercise 2 Add varian exercise 2/B

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,20 @@ const number = 100 //Number
 console.log(number);
 
 // EXERCICE 2
-//     - Declare constant identify as PI with value 4.14
+//     - Declare constant identify as PI with value 3.14
 //     - Print in console
 
-
+const PI = 3.14 //number
+console.log(PI);
 
 // EXERCICE 2 / B
 //      - Assigne to PI value 3.1416.
 //      - What happen ?
 
-
+//"you can't change the value of a constant because that would be an error"
+const PI = 3.1416
+console.log(PI);
+// Console message: Uncaught SyntaxError: Identifier 'PI' has already been declared (at main.js:20:7)//
 
 //  EXERCICE 2 / C
 //      - Declare variable identify RADIUS with value 8
